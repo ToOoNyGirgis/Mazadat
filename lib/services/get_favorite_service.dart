623d0e1f8c5d4f30dc37a04dccbcad6213687ad5
tonygirgis <tonygirgis29@gmail.com>
2023-05-03 13:03:47 +0300

    add favorite service and get category item service

---
+++ lib/services/get_favorite_service.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_app/models/items_model.dart';
+
+class FavoriteService {
+  late Map<String, String> requestHeaders;
+
+  static const _kApiUrl = 'https://mazadat.bluesoftec.net/api/favorites/';
+
+  get_headers(BuildContext context) async {
+    return requestHeaders = {
+      'Content-type': 'application/json',
+      'lang': context.locale.toString(),
+      'token': "token"
+    };
+  }
+
+  Future<List<dynamic>> getCategories(BuildContext context) async {
+    try {
+      http.Response response = await http.get(Uri.parse(_kApiUrl + 'getAll'),
+          headers: await get_headers(context));
+
+      if (response.statusCode != 200) {
+        var data = jsonDecode(response.body);
+        throw Exception(data['error']['message']);
+      }
+      final items = json.decode(response.body)['data'] as List?;
+      List<ItemsModel> list =
+      items!.map((val) => ItemsModel.fromJson(val)).toList();
+
+      return list;
+    } on SocketException {
+      throw Exception('No Internet connection 😑');
+    } on HttpException {
+      throw Exception("Couldn't find the post 😱");
+    } on FormatException {
+      throw Exception("Bad response format 👎");
+    }
+  }
+}
