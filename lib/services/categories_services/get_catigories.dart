dd13d0e2c36809e1a5bf0d4cc4cc113bff061fa7
tonygirgis <tonygirgis29@gmail.com>
2023-05-03 11:34:53 +0300

    add categories services and user services

--- lib/services/categories_services/get_catigories.dart
+++
@@ -1,9 +0,0 @@
-import 'package:news_app/helper/api.dart';
-
-class CategoriesService {
-  Future<List<dynamic>> getCategories() async {
-    List<dynamic> data =
-    await Api().get(url: '');
-    return data;
-  }
-}
