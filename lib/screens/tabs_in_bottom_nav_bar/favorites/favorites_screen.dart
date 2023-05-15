fea3a4f4d4f3147bb4b4451b2ab9720fda0f90ba
tonygirgis <tonygirgis29@gmail.com>
2023-05-15 15:10:50 +0300

    view listview inside listview to show the data that the user make it favorite

--- lib/screens/tabs_in_bottom_nav_bar/favorites/favorites_screen.dart
+++ lib/screens/tabs_in_bottom_nav_bar/favorites/favorites_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/common/constant.dart';
 import 'package:news_app/helper/sqldb.dart';
-import 'package:news_app/screens/tabs_in_bottom_nav_bar/favorites/widgets/favorites_item.dart';
+import 'package:news_app/models/items_model.dart';
+import 'package:news_app/screens/tabs_in_bottom_nav_bar/category/widgets/ItemsInFilter.dart';
+import 'package:news_app/services/get_item_service.dart';
 import 'package:sizer/sizer.dart';
 
 class FavoritesScreen extends StatefulWidget {
@@ -20,6 +22,8 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
     return response;
   }
 
+  List<ItemsModel> items = [];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,15 +35,52 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
           builder: (context, AsyncSnapshot<List<Map>> snapshot) {
             if (snapshot.hasData) {
               return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    print(snapshot.data![index]['categoryName']);
-                    return FavoritesItem(
-                      category: '${snapshot.data![index][kCategoryNameDB]}',
-                      city: '${snapshot.data![index][kCityNameDB]}',
-                    );
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FutureBuilder(
+                        future: ItemService().filter(context,{
+                          'category_id': snapshot.data![index][kCategoryIdDB].toString(),
+                          'city_id': snapshot.data![index][kCityIdDB],
+                        }),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<dynamic> FilterSnapshot) {
+                          print(FilterSnapshot.data);
+                          if (FilterSnapshot.hasData) {
 
-                  });
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: FilterSnapshot.data!.length,
+                              itemBuilder: (context, filterIndex) {
+                                return Padding(
+                                  padding: const EdgeInsets.only(top: 20),
+                                  child: ItemsInFilter(items: FilterSnapshot.data[filterIndex]),
+                                );
+                              },
+                            );
+                          }
+                          else {
+                            return const Center(child: CircularProgressIndicator());
+                          }
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+              // return ListView.builder(
+              //     itemCount: snapshot.data!.length,
+              //     itemBuilder: (context, index) {
+              //       print(snapshot.data![index]['categoryName']);
+              //       return FavoritesItem(
+              //         category: '${snapshot.data![index][kCategoryNameDB]}',
+              //         city: '${snapshot.data![index][kCityNameDB]}',
+              //       );
+              //
+              //     });
             }
             return const Center(
               child: CircularProgressIndicator(),
